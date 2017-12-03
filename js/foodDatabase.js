f772a69ff7d2b1fdbb910649a19133ff8ef98720
shaunmcc55 <shaunmcc55@gmail.com>
2017-12-03 09:19:46 +0200

    shaun foods

--- js/foodDatabase.js
+++ js/foodDatabase.js
@@ -3,3 +3,31 @@ var food = [];
 //Here is the format 
 //food['apple'] = [(calories),(fat),(protein)]
 //food['apple'] = [15,30,20]
+food['tomato']+ [18,0,1];
+food['onion']+ [40,0,1];
+food['carrot']+ [41,0,1];
+food['avocado']+ [167,9,2];
+food['green Pepper']+ [20,0,1];
+food['garlic']+ [149,0,6];
+food['salt']+ [0,0,0];
+food['rice']+ [130,0,2];
+food['bread']+ [305,6,9];
+food['cucumber']+ [15,0,1];
+food['spaghetti']+ [124,1,5];
+food['potato']+ [94,0,1];
+food['parsley']+ [40,0,0];
+food['vinegar']+ [21,0,0];
+food['black pepper']+ [251,3,10];
+food['lemon']+ [22,0,0];
+food['peas']+ [81,0,5];
+food['broccoli']+ [34,0,3];
+food['lettuce']+ [15,0,1];
+food['egg']+ [166,12,11];
+food['corn']+ [86,1,3];
+food['pickle']+ [12,0,1];
+food['eggplant']+ [25,0,1];
+food['olive oil']+ [884,0,0];
+food['olive']+ [167,10,0];
+food['red pepper']+ [31,0,1];
+food['dough']+ [90,2,3];
+food['spinach']+ [23,0,3];
